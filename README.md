effcf1db040a50dd7404a5179eb4c4ed14fa2e63
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-13 22:07:08 +0100

    cleanup project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react-start-example
+
+## using css module
+* https://dev.to/0x96f/using-css-modules-with-create-react-app-22hd
--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import React, { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
 }
 
 export default App;
