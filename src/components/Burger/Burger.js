f1c5c70197e867e3843605a395b511e804c8ba61
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-14 11:28:49 +0100

    making the burger output dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,25 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // transform object to an array
+    const transformedIngredients =
+        Object
+            .keys(props.ingredients)
+            .map(ingredientKey => {
+                // currently we have a map of "key = number"
+                // => this should be transformed into an array containing the "number" of "keys"
+                //    for example: cheese=2 should be transformend into ["cheese", "cheese"]
+                return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                    // now we create a Component for each ingredient
+                    // we just use the index for the key of the Component
+                    return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+                })
+            });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
