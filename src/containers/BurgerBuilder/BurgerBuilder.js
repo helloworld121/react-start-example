0584e3bb1bfecf79479d8e7b60f1148757cf905c
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-14 21:20:20 +0100

    adding Backdrop.js to close Modal.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -102,6 +102,9 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         this.setState({purchasing: true});
     }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
 
     render() {
         // copy the ingredient object and add information if remove-button must be disabled
@@ -115,7 +118,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
