5b21c64be9dab5a4e09320a6cfe87ab9089d5a4b
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-15 16:52:28 +0100

    new component DrawerToggle.js to open/close SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,17 +8,28 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
 
+    sideDrawerToggleHandler = () => {
+        // this approach may lead to unexpected behavior
+        // => due to async way setState work
+        // this.setState({showSideDrawer: !this.state.showSideDrawer});
+
+        // to work with previous state use the funcation-way
+        this.setState((prevState) =>{
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    }
+
     render() {
         return (
             <Aux>
-                <Toolbar/>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
                     {this.props.children}
