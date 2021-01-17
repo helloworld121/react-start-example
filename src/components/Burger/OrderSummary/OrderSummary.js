3baec1082b868343a5ca7ffeb9620db32baa5c06
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-17 11:56:28 +0100

    optimization: update modal only if "show" changes (Change Detection)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object
-        .keys(props.ingredients)
-        .map(ingredientKey => {
-            return (
-                <li key={ingredientKey}>
-                    <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>: {props.ingredients[ingredientKey]}
-                </li>
-            );
-        });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout</p>
-            <Button btnType="Danger" clicked={props.purchasedCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchasedContinue}>CONTINUE</Button>
-        </Aux>
-    );
-};
+// we change OrderSummary to a class-based-component
+// => so we can add live-cycle-hooks
+// => we want to see when it gets rerendered
+class OrderSummary extends Component {
 
-export default orderSummary;
+    // after adding diese method with log-entry
+    // we will see, that the components gets updated even if we don't show it
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[OrderSummary] componentDidUpdate');
+    }
+
+    render() {
+        const ingredientSummary = Object
+            .keys(this.props.ingredients)
+            .map(ingredientKey => {
+                return (
+                    <li key={ingredientKey}>
+                        <span style={{textTransform: 'capitalize'}}>{ingredientKey}</span>: {this.props.ingredients[ingredientKey]}
+                    </li>
+                );
+            });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchasedCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchasedContinue}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+
+
+}
+
+export default OrderSummary;
