3ffbb1f8639b1063ad9039426418547909be5819
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-22 13:09:24 +0100

    render the generated form object in a generic way

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -77,15 +77,25 @@ class ContactData extends Component {
     }
 
     render () {
+        // convert the form-object into an array
+        // => so it is possible to loop over it
+        const formElementsArray = [];
+        for(let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key],
+            })
+        }
+
         let form = (
             <form>
-                {/*<Input inputtype="input" type="text" name="name" placeholder="Your Name"/>*/}
-                {/*<Input inputtype="input" type="email" name="email" placeholder="Your Mail"/>*/}
-                {/*<Input inputtype="input" type="text" name="street" placeholder="Street"/>*/}
-                {/*<Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>*/}
-
-                <Input elementType="" elementConfig="..." value="..." />
-
+                {formElementsArray.map((formElement) => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
