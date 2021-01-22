ab5c27ef94349bb3dd6f56cb9b8712f8f1ca63d2
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-22 10:06:42 +0100

    adjust Navigation using NavLink => and configure activeClassName and exact

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 import classes from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a
-            href={props.link}
-            className={props.active ? classes.active : null}>{props.children}</a>
+        {/*
+        1) the default active-classname is active
+            => but because we are using css-modules the class-names become unique
+            => therefore we need to pass it using activeClassName
+        2) url-matching
+            => because NavLink matches everything starting with the given "to"-value
+               we need to add the property exact to match the given paths we want
+        */}
+        <NavLink
+            to={props.link}
+            exact={props.matchExact}
+            activeClassName={classes.active}>{props.children}</NavLink>
     </li>
 );
 
